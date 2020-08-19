bbe12a5361b5e26e393710dd82d2c8f59c906dfd
Mikey Williams <manicdesigner@gmail.com>
2020-08-19 10:55:52 -0400

    add single linked list classes and methods

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,65 @@
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next  # next node in the list
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None  # points to first node in list
+        self.tail = None  # points to tail node in list
+        self.length = 0
+
+    def __str__(self):
+        pass
+
+    # append / add ==> add_to_tail
+    def add_to_tail(self, value):
+        # check if theres tail
+        # check if no tail (empty list)
+        if self.tail is None:
+            new_tail = Node(value, None)
+            # set head & tail to the new node
+            self.head = new_tail
+            self.tail = new_tail
+        # if there is a tail (general case):
+        else:
+            # create a new node with the value we want to add, next = None
+            new_tail = Node(value, None)
+            # set current tail.next to the new node
+            old_tail = self.tail
+            old_tail.next = new_tail
+            # set self.tail to the new node
+            self.tail = new_tail
+        self.length += 1
+    # remove
+
+    def remove_head(self):
+        # if not head (empty list)
+        if not self.head:
+            return None
+        # return none
+
+        # list with one element
+        if self.head == self.tail:
+            #  set self.head to current_head.next / None
+            current_head = self.head
+            self.head = None
+            # set tail to None
+            self.tail = None
+            # decrement
+            self.length -= 1
+            return current_head.value
+        else:
+            current_head = self.head
+            self.head = current_head.next
+            # return current_head.value
+            self.length = self.length - 1
+            return current_head.value
+
+    def remove_tail(self):
+        current_head = self.head
+        self.head = current_head.next
+        # return
