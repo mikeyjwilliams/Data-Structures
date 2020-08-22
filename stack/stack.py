1d822b74a6e51cd25b76e81aeb060f1ea37dc17e
Mikey Williams <manicdesigner@gmail.com>
2020-08-22 13:55:22 -0400

    add singly linked into stack couldnt figure any other way around

--- stack/stack.py
+++ stack/stack.py
@@ -1,7 +1,6 @@
 
-
-
-
+import time
+from singly_linked_list import LinkedList
 
 """
 A stack is a data structure whose primary purpose is to store and
@@ -23,11 +22,11 @@ class Stack:
         self.storage = []
 
     def __len__(self):
-        return self.size
+        return len(self.storage)
 
     def push(self, value):
         self.size += 1
-        return self.storage.append(value)
+        return self.storage.insert(0, value)
 
 
     def pop(self):
@@ -36,3 +35,18 @@ class Stack:
         else:
             self.size -= 1
             return self.storage.pop()
+
+
+n = 100000
+stack1 = Stack()
+start = time.time()
+for i in range(n):
+    stack1.push(i)
+print('push to back ', time.time() - start)
+
+n = 100000
+stack2 = Stack()
+start = time.time()
+for i in range(n):
+    stack2.pop()
+print('pop to back ', time.time() - start)
